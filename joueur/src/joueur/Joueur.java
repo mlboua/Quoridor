3e77e15c7b033a7373838bd2b500b359fceebf15
coulibaly sidiki <scoulib2@lx140.deptinfo-st.univ-fcomte.fr>
2014-04-30 16:49:16 +0200

    bug corrigé sur le joueur

--- joueur/src/joueur/Joueur.java
+++ joueur/src/joueur/Joueur.java
@@ -1,8 +1,5 @@
 package joueur;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -17,90 +14,53 @@ public class Joueur {
 	private static int COUP = 1;
 	public static void main(String[] args) {
 		Socket socket = null;
-		//BufferedReader in = null;
 		InputStream in = null;
-		//DataInputStream in = null;
 		OutputStream out;
 		Scanner sc = new Scanner(System.in);
-		byte[] req = new byte[100];
-		byte[] coup = new byte[100];
+		//byte[] req = new byte[34];
+		byte[] coup = new byte[28];
 		try {
 			socket = new Socket("localhost", 2014);
-			
 			System.out.println("Connexion établie");
-			
 			out = socket.getOutputStream();
 			in = socket.getInputStream();
-			
-			
-			byte[] type = intToByteArray(PARTIE);
-			for(int i = 0;i <type.length;i++){
-				req[i] = type[i];
-			}
 			
 			System.out.print("Nom du joueur : ");
 			String nom = sc.nextLine();
 						
-			byte[] nomB = nom.getBytes();
-			for(int i = 0; i< nomB.length;i++){
-				req[4+i] = nomB[i];
-			}
-			out.write(req);
-			out.flush();
+			demandePartie(out,PARTIE, nom);
+			PartiRep rep = startParti(in);
 			
-			//ByteBuffer bb = null;
-			ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
-			//byte[] content = new byte[ 2048 ];
-			int bytesRead = -1;
-			System.out.println("Taille data : "+in.available());
-			byte[] content = new byte[80];
-			//while( ( bytesRead = in.read( content ) ) != -1 ) {  
-				bytesRead = in.read(content);
-			    //baos.write( content, 0, bytesRead );  
-			//}
-			 
-			//ByteArrayInputStream bais = new ByteArrayInputStream( baos.toByteArray() );
-			byte[] repType = new byte[4];
-			byte[] coulB = new byte[4];
-			//byte[] nomA = new byte[50];
-			for(int i = 0; i < 4;i++)repType[i] = content[i];
-			for(int i = 0; i < 4;i++)coulB[i] = content[4+i];
-			//for(int i = 0; i < 4;i++)nomA[i] = content[8+i];
+
 			
-			int codeErr = getInt(repType);
-			int couleur = getInt(coulB);
-		
-			System.out.println("Demande de parti Error code : "+codeErr+" et couleur : "+couleur);
-			//out.write(1);
-			/*if(couleur == 0){*/
-				byte[] id = intToByteArray(1);
-				byte[] col = intToByteArray(0);
+			System.out.println("Demande de parti Error code : "+rep.err+" et couleur : "+rep.couleur+" nom :"+rep.nom);
+			if(rep.couleur == 0){
+				byte[] id = intToByteArray(5);
+				byte[] col = intToByteArray(Integer.reverseBytes(0));
 				byte[] typeCoup = intToByteArray(0);
-				byte[] axeN = intToByteArray(0);
+				byte[] axeN = intToByteArray(2);
 				byte[] axeA = intToByteArray(0);
 				for(int i = 0; i< id.length;i++)coup[i] = id[i];
 				for(int i = 0; i< col.length;i++)coup[id.length+i] = col[i];
 				for(int i = 0; i< typeCoup.length;i++)coup[8+i] = typeCoup[i];
 				for(int i = 0; i< axeN.length;i++)coup[12+i] = axeN[i];
 				for(int i = 0; i< axeA.length;i++)coup[16+i] = axeA[i];
-				out = socket.getOutputStream();
+
 				out.write(coup);
-				//out.write(1);
 				out.flush();
 				System.out.println("Coup envoyé !");
-			//}
-			//content = new byte[in.available()];
-			/*content = new byte[80];
-			bytesRead = in.read(content);
+			}
+			
+			byte[] validCoup = new byte[8];
+			int bytesRead = in.read(validCoup);
 			byte[] coupRep = new byte[4];
 			byte[] coupValide = new byte[4];
-			for(int i = 0; i < 4;i++)coupRep[i] = content[i];
-			for(int i = 0; i < 4;i++)coupValide[i] = content[4+i];
+			for(int i = 0; i < 4;i++)coupRep[i] = validCoup[i];
+			for(int i = 0; i < 4;i++)coupValide[i] = validCoup[4+i];
 			
 			int errCoup = getInt(coupRep);
 			int valCoup = getInt(coupValide);
-			
-			System.out.println("Error du coup "+errCoup+" validité "+valCoup);*/
+			System.out.println("Error du coup adv "+errCoup+" validité "+valCoup);
 			
 			socket.close();
 		} catch (UnknownHostException e) {
@@ -116,6 +76,7 @@ public class Joueur {
 	
 	//convertion d'un int en tableau de byte
 	public static final byte[] intToByteArray(int value) {
+		value = Integer.reverseBytes(value);
         return new byte[] {
                 (byte)(value >>> 24),
                 (byte)(value >>> 16),
@@ -129,5 +90,38 @@ public class Joueur {
 		buffer.order(ByteOrder.nativeOrder());
 		return buffer.getInt();
 	}
+	
+	//envoi d'une demande de partie
+	public static void demandePartie(OutputStream out,int type, String name){
+		byte[] req = new byte[34];
+		byte[] t = intToByteArray(type);
+		for(int i = 0;i <t.length;i++){
+			req[i] = t[i];
+		}
+		byte[] nom = name.getBytes();
+		for(int i = 0; i< nom.length;i++){
+			req[4+i] = nom[i];
+		}
+		try {
+			out.write(req);
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
+	public static PartiRep startParti(InputStream in) throws IOException{
+		int bytesRead = -1;
+		byte[] partiRep = new byte[40];
+		bytesRead = in.read(partiRep);
+		byte[] repType = new byte[4];
+		byte[] coulB = new byte[4];
+		for(int i = 0; i < 4;i++)repType[i] = partiRep[i];
+		for(int i = 0; i < 4;i++)coulB[i] = partiRep[4+i];	
+		int codeErr = getInt(repType);
+		int couleur = getInt(coulB);
+		String nomA = new String(partiRep,8,partiRep.length-8);
+			
+		return new PartiRep(codeErr, couleur, nomA);
+	}
 }
---
+++ joueur/src/joueur/PartiRep.java
@@ -0,0 +1,14 @@
+package joueur;
+
+public class PartiRep {
+	public int err;
+	public int couleur;
+	public String nom;
+	
+	public PartiRep(int err, int couleur, String name){
+		this.err = err;
+		this.couleur = couleur;
+		this.nom = name;
+	}
+
+}
